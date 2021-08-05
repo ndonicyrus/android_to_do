6114edae833cb01ff7cdd51df926a1a30cbbc2d3
ndonicyrus <ndonicyrus29@gmail.com>
2021-08-05 19:54:39 +0300

    saved recycler view

--- app/src/main/java/com/example/todoapp/adapters/NoteAdapter.java
+++ app/src/main/java/com/example/todoapp/adapters/NoteAdapter.java
@@ -1,15 +1,18 @@
 package com.example.todoapp.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.todoapp.R;
+import com.example.todoapp.ToDoDetailActivity;
 import com.example.todoapp.models.Note;
 
 import java.util.ArrayList;
@@ -41,6 +44,7 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
       holder.title.setText(todo.getTitle());
       holder.details.setText(todo.getDescription());
       holder.time.setText("1 hour ago");
+      holder.id = todo.getId();
 
     }
 
@@ -52,6 +56,7 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView title, details, time;
+        long id;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -59,8 +64,22 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
             details = itemView.findViewById(R.id.txt_todo_details);
             time = itemView.findViewById(R.id.txt_todo_time);
 
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    Toast.makeText(context,"Nitakufinya", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+            });
 
-
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, ToDoDetailActivity.class);
+                    intent.putExtra("ID", id);
+                    context.startActivity(intent);
+                }
+            });
 
         }
     }
